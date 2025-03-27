e2d943127a432bea724d755f40bd65c02352efc4
LenorSantos <monteirof700@gmail.com>
2025-03-27 20:55:50 -0300

    adicionado animação na Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,70 @@
+import { useEffect, useState } from "react";
+import ImgMainJnunes from "../../images/logo-header-jotanunes.png";
+import "../../styles/Home.css"
+
 export default function Home() {
+  const [rotation, setRotation] = useState(0);
+  const [borderRadius, setBorderRadius] = useState("13px");
+  // const [circle, setCircle] = useState("50px");
+  const [showImage, setShowImage] = useState(false);
+  const [fadeImage, setFadeImage] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotation(90);
+
+      setTimeout(() => {
+        setBorderRadius("50%");
+      }, 1000);
+
+      setTimeout(() => {
+        setFadeImage(true);
+      }, 2000);
+
+      setTimeout(() => {
+        setShowImage(true);
+      }, 2500);
+
+      setTimeout(() => {
+        setFadeImage(false);
+      }, 4000);
+
+      setTimeout(() => {
+        setShowImage(false);
+        setRotation(0);
+        setBorderRadius("13px");
+      }, 5000);
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <>
-      <p>Aqui fica a tela inicial</p>
+      <div className="home-animation">
+        <div className="container">
+          {!showImage ? (
+            <div
+              className="shape"
+              style={{
+                transform: `rotate(${rotation}deg)`,
+                borderRadius: borderRadius,
+              }}
+            ></div>
+          ) : (
+            <img
+              className={`image ${fadeImage ? "fade-in" : ""}`}
+              src={ImgMainJnunes}
+              alt="Imagem"
+            />
+          )}
+          {/* <img
+              className={`image ${fadeImage ? "fade-in" : ""}`}
+              src={ImgMainJnunes}
+              alt="Imagem"
+            /> */}
+        </div>
+      </div>
     </>
   );
 }
