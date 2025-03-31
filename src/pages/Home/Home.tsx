22919eae4099eff5205974abf6b6bd2f80e4ca82
LenorSantos <monteirof700@gmail.com>
2025-03-31 01:06:32 -0300

    ajustes na lógica da animação e adição da animação da imagem

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,69 +4,85 @@ import "../../styles/Home.css"
 import "../../styles/Animation-Home.css";
 
 export default function Home() {
-  const [rotation, setRotation] = useState(0);
-  const [borderRadius, setBorderRadius] = useState("13px");
   const [showImage, setShowImage] = useState(false);
-  const [fadeImage, setFadeImage] = useState(false);
-  const [animation, setAnimation] = useState("");
+  const [animationShape, setAnimationShape] = useState("");
+  const [animationImg, setAnimationImg] = useState("");
+
+  function startAnimation() {
+    setShowImage(false);
+    setTimeout(() => {
+      setAnimationShape("rotate-scale-down 1s ease-in-out both");
+
+      setTimeout(() => {
+        setAnimationShape("bounce-out-top 1.5s both");
+        setTimeout(() => {
+          setAnimationShape("");
+          setShowImage(true);
+          setAnimationImg("scale-in-ver-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both");
+  
+          setTimeout(() => {
+            setAnimationImg("");
+          }, 3000);
+  
+        }, 1600);
+      }, 1100);
+    }, 500);
+  }
 
   useEffect(() => {
-    // const interval = setInterval(() => {
-    //   setAnimation("rotate-scale-down 1s ease-in-out both");
-
-    //   setTimeout(() => {
-    //     setAnimation("bounce-out-top 1.5s both");
-    //   }, 1100);
-
-    //   setTimeout(() => {
-    //     setAnimation("");
-    //   }, 5000);
-    // }, 5500);
-
-    // return () => clearInterval(interval);
+    // Inicia toda a animação
+    startAnimation();
 
     setInterval(() => {
-      setAnimation("rotate-scale-down 1s ease-in-out both");
 
+      setShowImage(false);
       setTimeout(() => {
-        setAnimation("bounce-out-top 1.5s both");
-      }, 1100);
+        setAnimationShape("rotate-scale-down 1s ease-in-out both");
 
-      setTimeout(() => {
-        setAnimation("");
-      }, 5000);
-    }, 5500);
+        setTimeout(() => {
+          setAnimationShape("bounce-out-top 1.5s both");
+          setTimeout(() => {
+            setAnimationShape("");
+            setShowImage(true);
+            setAnimationImg("scale-in-ver-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both");
+
+            setTimeout(() => {
+              setAnimationImg("");
+            }, 2500);
+
+          }, 1600);
+        }, 1100);
+      }, 500);
+      
+    }, 5000);
   }, []);
   
   return (
     <>
       <div className="home-animation">
-        <div className="container" 
-          // style={{animation: animation}}
-        >
+        <div className="container">
           {!showImage ? (
-            <div
-              className="shape"
-              style={{
-                // transform: `rotate(${rotation}deg)`,
-                // borderRadius: borderRadius,
-                // height: circle,
-                // width: circle,
-                animation: animation,
-              }}
-            ></div>
+            <div className="shape" style={{ animation: animationShape, }} />
           ) : (
             <img
-              className={`image ${fadeImage ? "fade-in" : ""}`}
+              // className={`image ${fadeImage ? "fade-in" : ""}`}
+              className="imageAnimationHome"
               src={ImgMainJnunes}
               alt="Imagem"
+              style={{
+                animation: animationImg,
+              }}
             />
           )}
           {/* <img
-              className={`image ${fadeImage ? "fade-in" : ""}`}
+              // className={`image ${fadeImage ? "fade-in" : ""}`}
+              className="imageAnimationHome"
               src={ImgMainJnunes}
               alt="Imagem"
-            /> */}
+              style={{
+                animation: animationImg,
+              }}
+          /> */}
         </div>
       </div>
     </>
