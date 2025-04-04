4622f6a8ed7dd022057b7b92d7b78af05e2e35ed
LenorSantos <monteirof700@gmail.com>
2025-04-04 13:45:11 -0300

    adicionado imagem de fundo da Home Dashboard e realizado ajustes de css globais para responsividade

--- src/pages/Dashboard/Home/Home.tsx
+++ src/pages/Dashboard/Home/Home.tsx
@@ -1,4 +1,35 @@
+import { useEffect } from "react";
+// import "../../../styles/dashboard/Home.css";
+import fundoDashboard from "../../../images/fundoDashboard.png";
+
 export default function HomeDashboard() {
+
+  useEffect(() => {
+    const style = document.createElement('style');
+    /*
+      @media (min-width: 768px) {
+        main {
+          padding: 12em 2% 2% 2%;
+        }
+
+        .header {
+          height: 0;
+        }
+      }
+    */
+    style.innerHTML = `
+      main {
+        background: url(${fundoDashboard});
+      }
+    `;
+    document.head.appendChild(style);
+
+    // remover o CSS quando o componente for desmontado ou a rota mudar
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
   return (
     <>
       <p>Home Dashboard</p>
