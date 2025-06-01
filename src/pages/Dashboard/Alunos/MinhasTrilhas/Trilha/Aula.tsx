3ddc933c1d8bb3cce48e17bf9fcdd00a703f1e76
Rafael <antonyrafaelsouzaa@gmail.com>
2025-06-01 10:44:50 -0300

    estrutura inicial da página

--- src/pages/Dashboard/Alunos/MinhasTrilhas/Trilha/Aula.tsx
+++ src/pages/Dashboard/Alunos/MinhasTrilhas/Trilha/Aula.tsx
@@ -1,7 +1,27 @@
 export default function Aula() {
   return (
     <>
-      <p>Aula do aluno</p>
+      <div className="container-aula">
+        <div className="container-lateral-aula">
+          <div className="modulo-aula"></div>
+          <div className="aulas"></div>
+          <div className="quiz-aula"></div>
+        </div>
+        <div className="container-video-aula">
+          <div className="titulo-aula"></div>
+          <div className="video-aula">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/sIjEzqbD2jg?si=sX2Cu-sRRQlPhYp7"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+          </div>
+          <div className="descricao-aula"></div>
+          <div className="navegacao-aula"> </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
