3134d643327ece82da13e9e423bb2278a6970d24
Rafael <antonyrafaelsouzaa@gmail.com>
2025-06-02 07:10:08 -0300

    Estilização inicial da página

--- src/pages/Dashboard/Alunos/MinhasTrilhas/Trilha/Aula.tsx
+++ src/pages/Dashboard/Alunos/MinhasTrilhas/Trilha/Aula.tsx
@@ -1,14 +1,33 @@
+import "../../../../../styles/dashboard/aluno/Aula.css";
+
 export default function Aula() {
   return (
     <>
       <div className="container-aula">
         <div className="container-lateral-aula">
-          <div className="modulo-aula"></div>
-          <div className="aulas"></div>
-          <div className="quiz-aula"></div>
+          <div className="modulo-aula">
+            <p>Segurança do Trabalho: Mólulo 1 - [40HS]</p>
+          </div>
+          <div className="aulas">
+            <div>
+              <p>[ ]Introdução a Segurança do Trabalho</p>
+            </div>
+            <div>
+              <p>[ ]Normas e Segurança do Trabalho</p>
+            </div>
+            <div>
+              <p>[ ]Prevenção e Controle de Sinistros</p>
+            </div>
+          </div>
+          <div className="quiz-aula">
+            <p>Quiz - Segurança do Trabalho: Módulo 01</p>
+          </div>
         </div>
         <div className="container-video-aula">
-          <div className="titulo-aula"></div>
+          <div className="informacao-aula">
+            <h1>Introdução</h1>
+            <p>Segurança do Trabalho: Módulo 01 [40 HORAS] Introdução</p>
+          </div>
           <div className="video-aula">
             <iframe
               width="560"
@@ -18,8 +37,18 @@ export default function Aula() {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             ></iframe>
           </div>
-          <div className="descricao-aula"></div>
-          <div className="navegacao-aula"> </div>
+          <div className="descricao-aula">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ea commodo consequat. Duis aute irure dolor in r
+            </p>
+          </div>
+          <div className="navegacao-aula">
+            <p>Modulo Anterior</p>
+            <p>Próxima Aula</p>
+          </div>
         </div>
       </div>
     </>
